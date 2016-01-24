8d6ba245b7edbe3c18011ee9717f11508feecde3
Nick Soranno <nsoranno10@winona.edu>
2016-01-23 20:07:12 -0600

    Additions   - Vendor folder and all subdirectories   - Foundation and support for it

---
+++ project/resources/views/css/styles.blade.php
@@ -0,0 +1,1 @@
+<link rel="stylesheet" href="/project/resources/views/Foundation6/css/foundation.css">
---
+++ project/resources/views/layouts/master_layout.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        @include('javascript.js')
+        @include('css.styles')
+        <title>Test!</title>
+    </head>
+    <body>
+
+    </body>
+</html>
