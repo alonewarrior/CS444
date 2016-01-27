624b53cf298cf5e69771fd187063ad649e79a7bd
znewell <znewell13@winona.edu>
2016-01-27 17:35:18 -0600

    HipChat
    added hipchat name

--- project/resources/views/test/home_test.blade.php
+++ project/resources/views/test/home_test.blade.php
@@ -3,7 +3,10 @@
 @section('title', 'Hello World')
 
 @section('main_content')
-    <h3>This is my page</h3>
+    <h2>This is my page</h2>
+
+    <h3>Hi everyone, I made a link/page for us to join on HipChat</h3>
+    <p>When you join HipChat join the group name "Cs444-kidsfirst" without the quotes!</p>
 
     <p>Zach Newell</p>
     <p>Blake Smola</p>
