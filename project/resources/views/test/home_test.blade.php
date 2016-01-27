f8c6335efb2dd70ea5dc63ec56e75272f492990d
znewell <znewell13@winona.edu>
2016-01-27 17:38:28 -0600

    Hipchat

--- project/resources/views/test/home_test.blade.php
+++ project/resources/views/test/home_test.blade.php
@@ -5,7 +5,7 @@
 @section('main_content')
     <h2>This is my page</h2>
 
-    <h3>Hi everyone, I made a link/page for us to join on HipChat</h3>
+    <h3>Hi everyone, I made a page for us to join on HipChat</h3>
     <p>When you join HipChat join the group name "Cs444-kidsfirst" without the quotes!</p>
 
     <p>Zach Newell</p>
