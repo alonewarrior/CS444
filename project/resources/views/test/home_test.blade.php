429b2ff4075f4dabaed3019a56fb36dbc2348fab
jfrancioni <jfrancioni@winona.edu>
2016-01-27 21:58:16 -0600

    HipChat clarification

--- project/resources/views/test/home_test.blade.php
+++ project/resources/views/test/home_test.blade.php
@@ -7,6 +7,8 @@
 
     <h3>Hi everyone, I made a page for us to join on HipChat</h3>
     <p>When you join HipChat join the group name "Cs444-kidsfirst" without the quotes!</p>
+	
+	<p>Dr. Joan here. Hold off on that HipChat group until you get the word from the Proj. Management team.</p>
 
     <p>Zach Newell</p>
     <p>Blake Smola</p>
